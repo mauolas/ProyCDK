b3481149cb0034d46466c11475a36fe3f0489e94
mauolas <mau.olascoaga98@gmail.com>
2020-04-04 09:30:10 -0700

    Ajuste de odbc en dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM python:3.7-alpine
 
+RUN apk add unixodbc psqlodbc
+
 ADD  ./src /code
+
+RUN cat /code/odbcinst.ini > /etc/odbcinst.ini
 
 WORKDIR /code
 
--- src/WebService.py
+++ src/WebService.py
@@ -35,3 +35,5 @@ def getsess():
         return controller.getSession(email,passw)
     return
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
